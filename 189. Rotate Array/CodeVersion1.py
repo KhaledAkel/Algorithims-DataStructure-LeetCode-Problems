876cc3f813711853f74deba944f5fd163767e9d2
Khaled Akel <124167245+KhaledAkel@users.noreply.github.com>
2023-09-25 14:57:15 +0200

    Create CodeVersion1.py

---
+++ 189. Rotate Array/CodeVersion1.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        # Make a copy
+        temp=[]
+        k=k%len(nums)
+        temp=nums[-k:]
+        nums[k:]=nums[:-k]
+        nums[:k]=temp
