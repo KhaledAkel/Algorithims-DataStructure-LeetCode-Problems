e8dce28adffd376cf0b5dcc07decf46173c2acc7
Khaled Akel <124167245+KhaledAkel@users.noreply.github.com>
2023-09-25 23:09:36 +0200

    Create CodeVersion1.py

---
+++ 202. Happy Number/CodeVersion1.py
@@ -0,0 +1,16 @@
+class Solution:
+    def isHappy(self, n: int, visited=None) -> bool:
+        
+        if visited is None:
+            visited = set()  
+    
+        if n == 1:
+            return True
+        
+        if n in visited:
+            return False
+        
+        visited.add(n) 
+        new_num = sum(int(digit) ** 2 for digit in str(n))
+        
+        return self.isHappy(new_num, visited)
