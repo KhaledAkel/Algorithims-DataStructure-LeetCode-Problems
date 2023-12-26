b28fedc69ec1bef053507f147a3a276fa64ab708
Khaled Akel <124167245+KhaledAkel@users.noreply.github.com>
2023-12-26 14:14:32 +0100

    Create CodeVersion1.py

---
+++ 104. Maximum Depth of Binary Tree/CodeVersion1.py
@@ -0,0 +1,36 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+
+        if not root:
+            return 0
+        MAX = -1
+
+        #DFS Using Stack 
+        stack = [(root,1)]
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                MAX = max(MAX, depth)
+                if node.left:
+                    stack.append((node.left, depth+1))
+                if node.right:
+                    stack.append((node.right, depth+1))
+
+        return MAX
+
+
+
+
+
+
+
+
+        
