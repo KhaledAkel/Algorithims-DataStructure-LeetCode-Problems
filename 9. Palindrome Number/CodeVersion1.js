152357b803cf8950d4efd0468114fbfcd0a1d120
Khaled Akel <124167245+KhaledAkel@users.noreply.github.com>
2024-05-15 14:29:10 +0200

    Update CodeVersion1.js

--- 9. Palindrome Number/CodeVersion1.js
+++ 9. Palindrome Number/CodeVersion1.js
@@ -1,1 +1,20 @@
 
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    // Convert the number to a string
+    const num = x.toString();
+
+    // Check if the number is negative
+    if (num[0] === '-') {
+        return false;
+    }
+
+    // Reverse the string
+    const rev = num.split('').reverse().join('');
+
+    // Check if the reversed string is equal to the original string
+    return num === rev;
+};
